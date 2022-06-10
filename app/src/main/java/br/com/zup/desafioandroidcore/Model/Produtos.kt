8afc3ac9122c326f445eb43bdf062943ffe400b3
Luciano Garcia <lualgoritmo@outlook.com>
2022-06-10 20:55:38 -0300

    Finalizado o Layout Desafio Android

---
+++ app/src/main/java/br/com/zup/desafioandroidcore/Model/Produtos.kt
@@ -0,0 +1,13 @@
+package br.com.zup.desafioandroidcore.Model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class Produtos(private var nomeProduto:String, private var quantidadeProduto:Int,
+private var valorUnitProduto:Double, private var receita:String): Parcelable {
+    fun getNomeProduto() = this.nomeProduto
+    fun quantidadeProduto() = this.quantidadeProduto
+    fun valorUnitProduto() = this.valorUnitProduto
+    fun getReceita() = this.receita
+}
