9aaf46b102347af2de2e51ea0069412985d1782a
Luciano Garcia <lualgoritmo@outlook.com>
2022-06-12 23:03:05 -0300

    Terminando o recyclerview e com erro no botao de cadastrar

--- app/src/main/java/br/com/zup/desafioandroidcore/Fragments/FragmentoProdutosCadastrados.kt
+++ app/src/main/java/br/com/zup/desafioandroidcore/Fragments/FragmentoProdutosCadastrados.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.zup.desafioandroidcore.Fragments.Adapter.ProdutoAdapter
+import br.com.zup.desafioandroidcore.Model.Produto
 import br.com.zup.desafioandroidcore.R
 import br.com.zup.desafioandroidcore.databinding.FragmentProdutosCadastradosBinding
 
@@ -23,6 +24,25 @@ class ProdutosCadastrados : Fragment() {
     ): View {
         binding = FragmentProdutosCadastradosBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adicionarListaProdutos()
+    }
+    private fun adicionarListaProdutos() {
+        var listaProduto = mutableListOf<Produto>()
+        var novaLista = arguments?.getParcelableArrayList<Produto>("PRODUTO")
+
+        if (novaLista != null) {
+           if (listaProduto.size == 0) {
+               listaProduto = novaLista
+           }
+        }
+        else {
+            novaLista?.let { listaProduto.addAll(it) }
+        }
+        produtoAdapter.atualizarListaProduto(listaProduto)
+        exibirRecycler()
     }
     private fun exibirRecycler(){
         binding.rvListaProdutosCadastrados.adapter = produtoAdapter
